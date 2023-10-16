c503f788e28285791f4e4421833f25e82c0ffcb6
AncientLust <aliakseikartashou@gmail.com>
2023-10-16 22:46:43 +0200

    Module 7 Task 1 completed

---
+++ Module 7/DataStructures/Tasks/DoublyLinkedListEnumerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Tasks;
+
+public partial class DoublyLinkedList<T>
+{
+    private class DoublyLinkedListEnumerator : IEnumerator<T>
+    {
+        private readonly DoublyLinkedList<T> _list;
+        private Node _currentNode;
+
+        public T Current => _currentNode.Data;
+
+        object IEnumerator.Current => Current;
+
+        public DoublyLinkedListEnumerator(DoublyLinkedList<T> list)
+        {
+            _list = list;
+            _currentNode = null;
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            _currentNode = _currentNode is null ? _list.HeadNode : _currentNode.Next;
+            return _currentNode is not null;
+        }
+
+        public void Reset()
+        {
+            _currentNode = null;
+        }
+    }
+}
---
+++ Module 7/DataStructures/Tasks/Node.cs
@@ -0,0 +1,36 @@
+﻿namespace Tasks;
+
+public partial class DoublyLinkedList<T>
+{
+    public class Node
+    {
+        public T Data;
+        public Node Next;
+        public Node Previous;
+
+        public Node(T data)
+        {
+            Data = data;
+            Previous = null;
+        }
+
+        public Node(T data, Node previousNode)
+        {
+            Data = data; 
+            Previous = previousNode;
+        }
+
+        public void AddToEnd(Node previousNode, T data)
+        {
+            if (Next is null)
+            {
+                Next = new Node(data, previousNode);
+            }
+            else
+            {
+                Next.AddToEnd(this, data);
+            }
+        }
+    }
+
+}
