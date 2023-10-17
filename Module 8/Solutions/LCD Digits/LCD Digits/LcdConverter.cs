a4553a28ff3543271a232773b36acec8883d8ab7
AncientLust <aliakseikartashou@gmail.com>
2023-10-17 18:18:06 +0200

    Revert "Implement solution for LCD Digits"
    This reverts commit 3894e95ab5081938bea1d59b9e7d141fffd00ee6.

--- Module 8/Solutions/LCD Digits/LCD Digits/LcdConverter.cs
+++ Module 8/Solutions/LCD Digits/LCD Digits/LcdConverter.cs
@@ -2,25 +2,8 @@
 
 public class LcdConverter
 {
-    private static readonly List<string> Digit = new()
-    {
-        "._.\n|.|\n|_|", // 0
-        "...\n..|\n..|", // 1
-        "._.\n._|\n|_.", // 2
-        "._.\n._|\n._|", // 3
-        "...\n|_|\n..|", // 4
-        "._.\n|_.\n._|", // 5
-        "._.\n|_.\n|_|", // 6
-        "._.\n..|\n..|", // 7
-        "._.\n|_|\n|_|", // 8
-        "._.\n|_|\n..|", // 9
-    };
-
     public string Convert(int digit)
     {
-        if (digit is < 0 or > 9)
-            throw new ArgumentOutOfRangeException();
-
-        return Digit[digit];
+        throw new NotImplementedException();
     }
 }
