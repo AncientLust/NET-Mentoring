f1819d49328a5ec528d241e3f08e0e2ebdc52a94
AncientLust <aliakseikartashou@gmail.com>
2023-10-17 02:50:56 +0200

    Module 7 Task 2 completed

---
+++ Module 7/DataStructures/Tasks/Task 2/HybridFlowProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Tasks.DoNotChange;
+
+namespace Tasks
+{
+    public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
+    {
+        private DoublyLinkedList<T> _storage = new();
+
+        public T Dequeue()
+        {
+            if (_storage.Length == 0) throw new InvalidOperationException();
+            return _storage.RemoveAt(0);
+        }
+
+        public void Enqueue(T item)
+        {
+            _storage.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (_storage.Length == 0) throw new InvalidOperationException();
+            return _storage.RemoveAt(_storage.Length - 1);
+        }
+
+        public void Push(T item)
+        {
+            _storage.Add(item);
+        }
+    }
+}
