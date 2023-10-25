86309595948ce84987eb648814eab195ecc5eb7b
AncientLust <aliakseikartashou@gmail.com>
2023-10-25 23:07:46 +0200

    Module 11 Task 3 completed

---
+++ Module 11/Serialization/Task 3 Deep cloning/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_3_Deep_cloning;
+
+[Serializable]
+public class Employee
+{
+    public string EmployeeName { get; set; }
+
+    public Employee(string name)
+    {
+        EmployeeName = name;
+    }
+}
+
