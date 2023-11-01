9a2dc61ff7f99e73dc7a586996be0240b4de368b
AncientLust <aliakseikartashou@gmail.com>
2023-11-01 12:59:25 +0100

    Module 15 Task with Dapper completed

---
+++ Module 15/ORM Fundamentals Dapper/Library/Database connections/SqlServerConnector.cs
@@ -0,0 +1,28 @@
+﻿using Library.Interfaces;
+using Microsoft.Data.SqlClient;
+
+namespace Library.Database_connections;
+
+public class SqlServerConnector : IDatabaseConnector
+{
+    private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=orm_module_dapper;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private SqlConnection? _connection;
+
+    public SqlConnection OpenConnection()
+    {
+        _connection = new SqlConnection(ConnectionString);
+        _connection.Open();
+        return _connection;
+    }
+
+    public void CloseConnection()
+    {
+        _connection?.Close();
+        _connection?.Dispose();
+    }
+
+    public void Dispose()
+    {
+        CloseConnection();
+    }
+}
