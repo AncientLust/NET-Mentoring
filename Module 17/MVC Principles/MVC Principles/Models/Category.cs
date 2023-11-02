886f573eb94ca94a2d13e839bdffd546902b699d
AncientLust <aliakseikartashou@gmail.com>
2023-11-02 22:10:14 +0100

    Module 17 Task 2 completed

---
+++ Module 17/MVC Principles/MVC Principles/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC_Principles.Models;
+
+public class Category
+{
+    public int CategoryID { get; set; }
+    public string CategoryName { get; set; }
+    public string Description { get; set; }
+}
---
+++ Module 17/MVC Principles/MVC Principles/contexts/SqlServerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Principles.Models;
+
+namespace MVC_Principles.contexts;
+
+public class SqlServerContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+
+    public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+}
